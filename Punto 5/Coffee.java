41673026d828e8659c231bb8dee094a620d50544
StevenEsmar <stespinosama@unal.edu.co>
2019-11-15 11:53:54 -0500

    DecoratorCoffee :art:

---
+++ Punto 5/Coffee.java
@@ -0,0 +1,25 @@
+
+
+public class Coffee extends Beverage
+
+{
+
+    public Coffee()
+
+    {
+
+	description = "Coffee";
+
+    }
+
+    
+/*
+    public double cost()
+
+    {
+
+	return 1.99;
+
+    }
+*/
+}
